0a2bc1477cdf1656a6dc0c27e5f109c664da9d4d
Leidy_Castro <dayanacf3@gmail.com>
2021-06-10 10:41:47 -0500

    estado del 80% del proyecto

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Todo from './Components/Todo';
 import Cuerpo from './Components/Cuerpo';
 import Footer from './Components/Footer';
 import Title from './Components/Title'
+import GlobalStyles from './globalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Title />
       <Todo />
       <Cuerpo />
